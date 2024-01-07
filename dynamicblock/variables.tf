4956905b2216e0a3f917d9e995ab352d37b28e27
amu94777 <amu94777@gmail.com>
2024-01-07 11:55:40 +0530

    dynamic block

---
+++ dynamicblock/variables.tf
@@ -0,0 +1,32 @@
+variable "ingress-rules" {
+    default = [
+        {
+        description      = "roboshop-all"
+          from_port        = 80
+          to_port          = 80
+          protocol         = "tcp"
+          cidr_blocks      = ["0.0.0.0/0"]
+        },
+         
+         {
+         description      = "roboshop-all"
+          from_port        = 443
+          to_port          = 443
+          protocol         = "tcp"
+          cidr_blocks      = ["0.0.0.0/0"]
+        },
+
+
+        {
+         description      = "roboshop-all"
+          from_port        = 22
+          to_port          = 22
+          protocol         = "tcp"
+          cidr_blocks      = ["0.0.0.0/0"]
+        }
+        
+    ]
+} 
+  
+    
+    
